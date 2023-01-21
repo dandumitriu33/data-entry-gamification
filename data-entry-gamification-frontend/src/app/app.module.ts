c00f5856a3003fc81903f40f06f237d5afcdc489
Dan Dumitriu <dandumitriu33@gmail.com>
2023-01-21 16:18:42 +0200

    Add base paper sim entry source

--- data-entry-gamification-frontend/src/app/app.module.ts
+++ data-entry-gamification-frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AvatarComponent } from './components/avatar/avatar.component';
+import { PaperSimComponent } from './components/paper-sim/paper-sim.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AvatarComponent,
+    PaperSimComponent,
   ],
   imports: [
     BrowserModule,
---
+++ data-entry-gamification-frontend/src/app/components/paper-sim/paper-sim.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paper-sim',
+  templateUrl: './paper-sim.component.html',
+  styleUrls: ['./paper-sim.component.css']
+})
+export class PaperSimComponent {
+
+}
