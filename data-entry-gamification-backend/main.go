c889c8385a570a2f9c7df94db225d2072eb7c792
Dan Dumitriu <dandumitriu33@gmail.com>
2023-01-27 08:29:03 +0200

    Add Logout endpoint and logic

--- data-entry-gamification-backend/main.go
+++ data-entry-gamification-backend/main.go
@@ -54,7 +54,7 @@ func main() {
 	router.POST("/api/register", users.Register)
 	router.POST("/api/login", users.Login)
 	router.GET("/api/user", users.Get)
-	// router.GET("/api/logout", users.Logout)
+	router.GET("/api/logout", users.Logout)
 
 	router.Run("localhost:8080")
 }
