b4824e92c015a9445148fedd008696c62496df79
Dan Dumitriu <dandumitriu33@gmail.com>
2023-01-29 11:36:34 +0200

    Add login page functionality with jwt cookie set

--- data-entry-gamification-frontend/src/app/components/login/login.component.ts
+++ data-entry-gamification-frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+  loginUrl = "http://localhost:8080/api/login";
 
+  constructor(private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: '',
+      password: ''
+    })
+  }
+
+  submit(): void {
+    this.http.post(this.loginUrl, this.form.getRawValue(), {
+      withCredentials: true
+    }).subscribe(()=> this.router.navigate(['/']));
+  }
 }
