9efb7cb670b37425996c460e8e70df089c987395
Dan Dumitriu <dandumitriu33@gmail.com>
2023-01-29 11:47:47 +0200

    Add user retreival on home page with message

--- data-entry-gamification-frontend/src/app/components/home/home.component.ts
+++ data-entry-gamification-frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  message = '';
+  getUserUrl = "http://localhost:8080/api/user";
 
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.http.get(this.getUserUrl, {withCredentials: true}).subscribe(
+      (res: any) => {
+        this.message = `Hello ${res.first_name}`;
+      },
+      err => {
+        console.error(err);
+        this.message = "You are not logged in."
+      }
+    );
+  }
 }
