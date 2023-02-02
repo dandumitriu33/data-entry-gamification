109e65c3dfffd1c59b5619b8cfd97b99d40b9375
Dan Dumitriu <dandumitriu33@gmail.com>
2023-02-02 08:40:09 +0200

    Add controller method AddReceipt

--- data-entry-gamification-backend/controller/receipts/receipt_controller.go
+++ data-entry-gamification-backend/controller/receipts/receipt_controller.go
@@ -1,2 +1,28 @@
 package receipts
 
+import (
+	"data-entry-gamification/model"
+	"data-entry-gamification/service"
+	"data-entry-gamification/utils/errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AddReceipt(c *gin.Context) {
+	var receipt model.Receipt
+
+	if err := c.ShouldBindJSON(&receipt); err != nil {
+		err := errors.NewBadRequestError("invalid json body")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	result, saveErr := service.CreateReceipt(receipt)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, result)
+}
