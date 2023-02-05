170e5d0f2849e44351bca23416542c3eaf13de20
Dan Dumitriu <dandumitriu33@gmail.com>
2023-02-05 10:17:09 +0200

    Add user_receipts and user_points functionality

--- data-entry-gamification-backend/controller/receipts/receipt_controller.go
+++ data-entry-gamification-backend/controller/receipts/receipt_controller.go
@@ -5,8 +5,14 @@ import (
 	"data-entry-gamification/service"
 	"data-entry-gamification/utils/errors"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	jwt "github.com/golang-jwt/jwt/v4"
+)
+
+const (
+	SecretKey = "abc123"
 )
 
 func AddReceipt(c *gin.Context) {
@@ -16,9 +22,43 @@ func AddReceipt(c *gin.Context) {
 		err := errors.NewBadRequestError("invalid json body")
 		c.JSON(err.Status, err)
 		return
+	}	
+
+	// Add receipt and points to user
+	// Get User ID from JWT token (separate service and DB in future)
+	cookie, err := c.Cookie("jwt")
+	if err != nil {
+		getErr := errors.NewInternalServerError("could not retrieve cookie")
+		c.JSON(getErr.Status, getErr)
+		return
 	}
 
-	result, saveErr := service.CreateReceipt(receipt)
+	// token, err := jwt.ParseWithClaims(cookie, &jwt.RegisteredClaims{}, func(*jwt.Token) (interface{}, error) {
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(*jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+	if err != nil {
+		restErr := errors.NewInternalServerError("error parsing cookie")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	claims := token.Claims.(*jwt.StandardClaims)
+	issuer, err := strconv.ParseInt(claims.Issuer, 10, 64)
+	if err != nil {
+		restErr := errors.NewBadRequestError("user id should be a number")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	user, restErr := service.GetUserByID(issuer)
+	if restErr != nil {
+		c.JSON(restErr.Status, restErr)
+		return
+	} 
+
+	// Add Receipt and other transaction parts (points, level)
+	result, saveErr := service.CreateReceipt(c, receipt, *user)
 	if saveErr != nil {
 		c.JSON(saveErr.Status, saveErr)
 		return
@@ -26,6 +66,24 @@ func AddReceipt(c *gin.Context) {
 
 	c.JSON(http.StatusOK, result)
 }
+
+// func AddReceipt2(c *gin.Context) {
+// 	var receipt model.Receipt
+
+// 	if err := c.ShouldBindJSON(&receipt); err != nil {
+// 		err := errors.NewBadRequestError("invalid json body")
+// 		c.JSON(err.Status, err)
+// 		return
+// 	}
+
+// 	result, saveErr := service.CreateReceipt(receipt)
+// 	if saveErr != nil {
+// 		c.JSON(saveErr.Status, saveErr)
+// 		return
+// 	}
+
+// 	c.JSON(http.StatusOK, result)
+// }
 
 func GetAllCount(c *gin.Context) {
 	result, err := service.GetAllCount()
--- data-entry-gamification-backend/model/receipts_dao.go
+++ data-entry-gamification-backend/model/receipts_dao.go
@@ -1,18 +1,77 @@
 package model
 
 import (
+	"context"
 	"data-entry-gamification/storage/receipts_db"
 	"data-entry-gamification/utils/errors"
 	"time"
 )
 
 var (
-	queryInsertReceipt = "INSERT INTO receipts (model_year, make, vin, first_name, last_name, state, date_added) VALUES (?, ?, ?, ?, ?, ?, ?);"
-	queryGetAllCount = "SELECT COUNT(*) FROM receipts;"
-	queryGetAllCountToday = "SELECT COUNT(*) FROM receipts WHERE DATE(date_added) = DATE(NOW());"
+	queryInsertReceipt         = "INSERT INTO receipts (model_year, make, vin, first_name, last_name, state, date_added) VALUES (?, ?, ?, ?, ?, ?, ?);"
+	queryGetAllCount           = "SELECT COUNT(*) FROM receipts;"
+	queryGetAllCountToday      = "SELECT COUNT(*) FROM receipts WHERE DATE(date_added) = DATE(NOW());"
+	queryInsertUserIDReceiptID = "INSERT INTO user_receipts (user_id, receipt_id) VALUES (?, ?);"
+	queryGetUserPointsByUserID = "SELECT points FROM user_points WHERE user_id = ?;"
+	queryInsertNewUserPoints   = "INSERT INTO user_points (user_id, points, level) VALUES (?, ?, ?);"
+	queryUpdateUserPoints      = "UPDATE user_points SET points = ?, level = ? WHERE user_id = ?;"
 )
 
-func (receipt *Receipt) Save() *errors.RestErr {
+func (receipt *Receipt) Save(ctx context.Context, userID int64) *errors.RestErr {
+	tx, err := receipts_db.Client.BeginTx(ctx, nil)
+	if err != nil {
+		return errors.NewInternalServerError("database transaction error")
+	}
+	defer tx.Rollback()
+
+	// Add receipt and get the ID
+	currentTime := time.Now()
+	date_added := currentTime.Format("20060102150405")
+	addResult, addErr := tx.ExecContext(ctx, queryInsertReceipt, receipt.ModelYear, receipt.Make, receipt.Vin, receipt.FirstName, receipt.LastName, receipt.State, date_added)
+	if addErr != nil {
+		return errors.NewInternalServerError("database transaction add error")
+	}
+	receiptID, err := addResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError("database transaction receipt ID retrieval error")
+	}
+	receipt.ID = receiptID
+
+	// Add userID and receiptID to user_receipts table
+	_, pairErr := tx.ExecContext(ctx, queryInsertUserIDReceiptID, userID, receiptID)
+	if pairErr != nil {
+		return errors.NewInternalServerError("database transaction pair error")
+	}
+
+	// Get User points
+	var userPoints int64
+	getPointsErr := tx.QueryRowContext(ctx, queryGetUserPointsByUserID, userID).Scan(&userPoints)
+	if getPointsErr != nil {
+		return errors.NewInternalServerError("database transaction get points error")
+	}
+
+	if userPoints == 0 {
+		// Insert User Points - first user input recording
+		_, pointsErr := tx.ExecContext(ctx, queryInsertNewUserPoints, userID, 1, 0)
+		if pointsErr != nil {
+			return errors.NewInternalServerError("database transaction add new points error")
+		}
+	} else {
+		// Update User Points
+		_, pointsErr := tx.ExecContext(ctx, queryUpdateUserPoints, userPoints+1, 0, userID)
+		if pointsErr != nil {
+			return errors.NewInternalServerError("database transaction add points error")
+		}
+	}	
+
+	// Commit the transaction.
+	if err = tx.Commit(); err != nil {
+		return errors.NewInternalServerError("database transaction commit error")
+	}
+	return nil
+}
+
+func (receipt *Receipt) Save2() *errors.RestErr {
 	stmt, err := receipts_db.Client.Prepare(queryInsertReceipt)
 	if err != nil {
 		return errors.NewInternalServerError("database error")
@@ -34,7 +93,7 @@ func (receipt *Receipt) Save() *errors.RestErr {
 }
 
 func (receipt *Receipt) GetAllCount() (int64, *errors.RestErr) {
-	var count int64	
+	var count int64
 
 	row := receipts_db.Client.QueryRow(queryGetAllCount)
 	err := row.Scan(&count)
@@ -46,7 +105,7 @@ func (receipt *Receipt) GetAllCount() (int64, *errors.RestErr) {
 }
 
 func (receipt *Receipt) GetAllCountToday() (int64, *errors.RestErr) {
-	var count int64	
+	var count int64
 
 	row := receipts_db.Client.QueryRow(queryGetAllCountToday)
 	err := row.Scan(&count)
