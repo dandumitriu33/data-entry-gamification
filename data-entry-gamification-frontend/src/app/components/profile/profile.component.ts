0c10ef729afd580f5e589720ad678c217e65712b
Dan Dumitriu <dandumitriu33@gmail.com>
2023-02-08 08:24:29 +0200

    Add Profile page Points and Level data from DB

--- data-entry-gamification-frontend/src/app/components/profile/profile.component.ts
+++ data-entry-gamification-frontend/src/app/components/profile/profile.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserInfo } from 'src/app/entities/user_info';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  userPoints = 0;
+  userLevel = 0;
+  getUserInfoUrl = "http://localhost:8080/api/user/info";
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.http.get(this.getUserInfoUrl, {withCredentials: true}).subscribe(
+      (res: any) => {
+        this.userPoints = res.points;
+        this.userLevel = res.level;
+      },
+      err => {
+        console.error(err);        
+      }
+    );
+  }
 
 }
