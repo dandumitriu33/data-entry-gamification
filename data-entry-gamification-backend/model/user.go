5ac7304e6d649838603baaf193d2a61111507470
Dan Dumitriu <dandumitriu33@gmail.com>
2023-03-09 07:52:17 +0200

    Add mapping functions for User

--- data-entry-gamification-backend/model/user.go
+++ data-entry-gamification-backend/model/user.go
@@ -23,3 +23,41 @@ type UserDAO struct {
 	Password  string 
 	Email     string 
 }
+
+// Maps the UserDTO struct data to User struct data to use internally.
+// Should be used on operation when other Roles pass user data not the user.
+// The user on password update would use the User struct.
+func mapFromUserDTOToModel(userDTO UserDTO, user *User) {
+	user.ID = userDTO.ID
+	user.FirstName = userDTO.FirstName
+	user.LastName = userDTO.LastName
+	user.Email = userDTO.Email
+}
+
+// Maps the User struct data to UserDTO struct data to use externally.
+// Should be used on operation when other Roles pass user data not the user.
+// The user on password update would use the User struct.
+func mapFromUserModelToDTO(user User, userDTO *UserDTO) {
+	userDTO.ID = user.ID
+	userDTO.FirstName = user.FirstName
+	userDTO.LastName = user.LastName
+	userDTO.Email = user.Email
+}
+
+// Maps the User with DB data to the User struct
+func mapFromUserDAOToModel(userDAO UserDAO, user *User) {
+	user.ID = userDAO.ID
+	user.Password = userDAO.Password
+	user.FirstName = userDAO.FirstName
+	user.LastName = userDAO.LastName
+	user.Email = userDAO.Email
+}
+
+// Maps the UserDAO with User data to insert in the DB 
+func mapFromUserModelToDao(user User, userDAO *UserDAO) {
+	userDAO.ID = user.ID
+	userDAO.Password = user.Password
+	userDAO.FirstName = user.FirstName
+	userDAO.LastName = user.LastName
+	userDAO.Email = user.Email
+}
