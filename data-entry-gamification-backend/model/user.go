28fc342273964ebd04ff110acfec732a6f7b4222
Dan Dumitriu <dandumitriu33@gmail.com>
2023-03-09 07:39:36 +0200

    Add userDTO and userDAO

--- data-entry-gamification-backend/model/user.go
+++ data-entry-gamification-backend/model/user.go
@@ -1,9 +1,25 @@
 package model
 
+// We keep the JSON notation bcause on Register we use this struct to pass the pwd
 type User struct {
 	ID        int64  `json:"ID"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Password  string `json:"password"`
 	Email     string `json:"email"`
+}
+
+type UserDTO struct {
+	ID        int64  `json:"ID"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+}
+
+type UserDAO struct {
+	ID        int64  
+	FirstName string 
+	LastName  string 
+	Password  string 
+	Email     string 
 }
