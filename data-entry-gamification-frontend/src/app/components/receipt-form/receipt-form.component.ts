8ed3f176d35f750d9e9154944bd251eab7f1acca
Dan Dumitriu <dandumitriu33@gmail.com>
2023-03-24 07:42:05 +0200

    Add Vin FE validation

--- data-entry-gamification-frontend/src/app/components/receipt-form/receipt-form.component.ts
+++ data-entry-gamification-frontend/src/app/components/receipt-form/receipt-form.component.ts
@@ -18,7 +18,7 @@ export class ReceiptFormComponent implements OnInit {
   receiptFormGroup = this.fb.group({
     model_year_reactive: ['', [Validators.required, Validators.min(1800), Validators.max(2200), Validators.pattern(/^\d{4}$/)]],
     make_reactive: ['', Validators.required],
-    vin_reactive: ['', Validators.required],
+    vin_reactive: ['', [Validators.required, Validators.minLength(17), Validators.maxLength(17)]],
     first_name_reactive: ['', [Validators.required, this.forbiddenNameValidator(/bob/i)]],
     last_name_reactive: ['', Validators.required],
     state_reactive: ['', Validators.required],
